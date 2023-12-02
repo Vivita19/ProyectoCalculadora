bba781afe03bce2277b93d295eaaf2011febc96f
Vivita19 <151692976+Vivita19@users.noreply.github.com>
2023-12-02 19:00:42 -0300

    Programando Paraguay

--- javascript.js
+++ javascript.js
@@ -1,41 +1,45 @@
 
-window.addEventListener('load', ()=>{ /*Escuchamps cuando se carga el documento */
+window.addEventListener('load', () => {
+    const display = document.querySelector('.calculator-display');
+    const keypadButtons = document.querySelectorAll('.keypad-button');
 
-    const display= document.querySelector('.calculator-display');
-    const keypadButton= document.getElementsByClassName('calculator-keypad');
+    const keypadButtonsArray = Array.from(keypadButtons);
 
-    const keypadButtonsArray= Array.from(keypadButton);
-
-    keypadButtonsArray.forEach ( (button)=> {
-
-        button.addEventListener('click', () =>{
-            console.log(button.innerHTML);
+    keypadButtonsArray.forEach((button) => {
+        button.addEventListener('click', () => {
+            calculadora(button, display);
         });
-    })
-    }
-) 
-function calculadora(button, display){
-    switch (button.innerHTML){
-        case 'C':
-            boorar(display)
-            break;
-        case '=':
-            calculadora(display);
-            break;
+    });
 
-        default:
-            actualizar(display, button);
-            break;
+    function calculadora(button, display) {
+
+        switch (button.innerHTML) {
+            case 'C':
+                borrar(display);
+                break;
+            case '=':
+                calcular(display);
+                break;
+            default:
+                actualizar(display, button);
+                break;
+        }
     }
-    function calcular(display){
-        display.innerHTML = eval(display.innerHTML)
+
+    function calcular(display) {
+        display.innerHTML = eval(display.innerHTML);
     }
-    function actualizar(display, button){
-        if (display.innerHTML == 0){
+
+    function borrar(display) {
+        display.innerHTML = '0';
+    }
+
+  
+    function actualizar(display, button) {   
+        if (display.innerHTML === '0') {
             display.innerHTML = '';
         }
-        display.innerHTML += button.innerHTML;
-    }
-  
-}
 
+        display.innerHTML += button.innerHTML;
+}
+});
